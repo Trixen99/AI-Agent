8f61ba7b10cca2288f0a3c2a75fdbc0c2ecf07c6
Trixen99 <NathanWhitehead99@hotmail.com>
2025-06-13 21:48:15 +0100

    finished get_files_info function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,18 +3,27 @@ import os
 
 def get_files_info(working_directory, directory=None):
     if os.path.commonpath([os.path.abspath(working_directory), os.path.abspath(os.path.join(working_directory, directory or ""))]) != os.path.abspath(working_directory):
-        print(os.path.commonpath([os.path.abspath(working_directory), os.path.abspath(os.path.join(working_directory, directory or ""))]))
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    #working_path = os.path.abspath(working_directory)
-    #directory_path = os.path.abspath(os.path.join(working_directory, directory or ""))
-    #if not directory_path.startswith(working_path):
-    #    return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-
-    #if not working_path.split("/")[-1] == directory_path.split("/")[-2]:
-    #    return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-
-
     
+    if not os.path.isdir(os.path.abspath(os.path.join(working_directory,directory))):
+        return ""
+    
+    
+    try:
+        directory_path = os.path.abspath(os.path.join(working_directory, directory))
+        return_string = []
+        directory_list = os.listdir(directory_path)
+        if len(directory_list) == 0:
+            return f"No files in directory: {directory_path}"
+        for item in directory_list:
+            item_path = os.path.join(directory_path, item)
+            return_string.append(f"- {item}: file_size={os.path.getsize(item_path)} bytes, is_dir={os.path.isdir(item_path)}")
+
+        return "\n".join(return_string)
+    except OSError as error:
+        return f"Error: {error}"
+
+
 
 
 
@@ -22,10 +31,3 @@ def get_files_info(working_directory, directory=None):
     
      
 
-
-
-
-print(get_files_info("/tmp/abc", "/tmp/abc2"))
-print("\n")
-#print(get_files_info("calculator", "/bin"))
-print(get_files_info("calculator", "pkg"))
